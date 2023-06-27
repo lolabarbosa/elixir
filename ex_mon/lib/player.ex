5b6d57eed25c5b4732dd463858a2511a8f0247cf
lolabarbosav <carolina.barboosa@gmail.com>
2023-06-26 21:26:06 -0300

    Improving attack code and change of turns

--- ex_mon/lib/player.ex
+++ ex_mon/lib/player.ex
@@ -1,8 +1,6 @@
 defmodule ExMon.Player do
-  required_keys = [:life, :move_avg, :move_heal, :move_rnd, :name]
+  required_keys = [:life, :moves, :name]
   @max_life 100
-
-  player = "banana"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
 
   @enforce_keys required_keys
   defstruct required_keys
@@ -10,12 +8,13 @@ defmodule ExMon.Player do
   def build(name, move_rnd, move_avg, move_heal) do
     %ExMon.Player{
       life: @max_life,
-      move_avg: move_avg,
-      move_heal: move_heal,
-      move_rnd: move_rnd,
+      moves: %{
+        move_avg: move_avg,
+        move_heal: move_heal,
+        move_rnd: move_rnd,
+      },
       name: name,
     }
   end
-  git config --global user.name "Seu nome"
 
 end
